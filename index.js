17e5855b744cd45ea6ce38c78e07b0219a618ec3
rounak adhikary <rounakadhikary1998@gmail.com>
2020-10-10 02:27:53 +0530

    testing mongoose with local rep

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 var app = require('express')();
+const mongoose = require('mongoose');
+
 const port = process.env.PORT || 3000;
+const database_url = 'mongodb://127.0.0.1:27017/flight';
+const coll_name='all_names';
+
+mongoose.connect(database_url, { useUnifiedTopology: true, useUnifiedTopology: true }).then(
+  () => {console.log('successfully database connected'); },
+  err => {console.log('error occured'); console.log(err); }
+);
+const fschema = new mongoose.Schema({
+	name : String,
+	title : String,
+	biggest_fear: String
+});
+const fmodel = mongoose.model(coll_name, fschema);
+fmodel.find().then((resp)=>{
+	console.log(resp);
+});
 
 app.get('/',function(req,res){
-	res.json({
-		'name' : 'ron',
-		'title' : 'weasley',
-		'biggest fear': 'spiders'
+	fmodel.find().then((resp)=>{
+		res.json(resp);
 	});
 });
 

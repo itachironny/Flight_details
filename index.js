40c333e8d0b0d3b460ba480b4eb5a5bb72ee5ec5
rounak adhikary <rounakadhikary1998@gmail.com>
2020-10-10 10:26:58 +0530

    Added mongoose lookup, but some errors are logged

--- index.js
+++ index.js
@@ -3,27 +3,47 @@ const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3000;
 const database_url = 'mongodb://127.0.0.1:27017/flight';
-const coll_name='all_names';
+const coll_name='all_flights';
 
-mongoose.connect(database_url, { useUnifiedTopology: true, useUnifiedTopology: true }).then(
+mongoose.connect(database_url, { useUnifiedTopology: true, useNewUrlParser: true }).then(
   () => {console.log('successfully database connected'); },
   err => {console.log('error occured'); console.log(err); }
 );
 const fschema = new mongoose.Schema({
-	name : String,
-	title : String,
-	biggest_fear: String
+	fnum : Number,
+	origin : String,
+	destination: String,
+	dep_time : String
 });
 const fmodel = mongoose.model(coll_name, fschema);
-fmodel.find().then((resp)=>{
-	console.log(resp);
+
+//get request to search flight by id
+app.get('/:Id',function(req,res){
+
+	var q = parseInt(req.params['Id']); //get the id and convert to integer
+	if(isNaN(q)){res.send("Not a valid id");res.end();} //check if the id has been successfully converted
+	
+	// try to find the flight with the id
+	try{
+		fmodel.findOne({'fnum':q}).then((resp)=>{
+			if(resp!=null){
+				res.json(resp);
+			}
+			else{
+				res.send("No such flight number exists"); res.end();
+			}
+		});
+	}
+	catch(err){
+		// console.log(err);
+		res.sendStatus(404); res.end();
+	}
+	
 });
 
 app.get('/',function(req,res){
-	fmodel.find().then((resp)=>{
-		res.json(resp);
-	});
+	res.send("Just a page");
 });
 
 
-app.listen(port,() => console.log('todo list RESTful API server started on: ' + port));
+app.listen(port,() => console.log('flight RESTful API server started on: ' + port));

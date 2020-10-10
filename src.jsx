23a2a81c08f583fb46576f1b3c059878af06bcfc
rounak adhikary <rounakadhikary1998@gmail.com>
2020-10-10 16:52:56 +0530

    Added a simple react frontend that can extract value from an input and display it in a paragraph on a button click

--- src.jsx
+++ src.jsx
@@ -8,24 +8,37 @@ class SearchBtn extends React.Component {
   }
 }
 
+class TextId extends React.Component {
+  render(){
+    return (
+      <input type="text" value={this.props.value} onChange={this.props.onChange}/>
+    );
+  }
+}
+
 class SearchDiv extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       query : null,
+      query_being_shown : null
     };
   }
 
   search(){
-    this.setState({query:"Hey there"});
+    this.setState({query_being_shown : this.state.query});
+  }
+  query_changed(event){
+    this.setState({query:event.target.value});
   }
   
   render() {
 
     return (
       <div className="searchdiv">
+        <TextId value={this.state.query} onChange={(event)=>{this.query_changed(event)}} />
         <SearchBtn onClick={()=>{this.search()}} />
-        <p>{this.state.query}</p>
+        <p>{this.state.query_being_shown}</p>
       </div>
     );
   }

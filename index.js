c75e173ad77fe44d78087ca0959c65d9c8398a24
rounak adhikary <rounakadhikary1998@gmail.com>
2020-10-10 10:42:21 +0530

    nothing

--- index.js
+++ index.js
@@ -18,28 +18,35 @@ const fschema = new mongoose.Schema({
 const fmodel = mongoose.model(coll_name, fschema);
 
 //get request to search flight by id
-app.get('/:Id',function(req,res){
+app.get('/:Id',
 
-	var q = parseInt(req.params['Id']); //get the id and convert to integer
-	if(isNaN(q)){res.send("Not a valid id");res.end();} //check if the id has been successfully converted
+	function(req,res,next){
+		//get the id and convert to integer
+		var q = parseInt(req.params['Id']);
+		//check if the id has been successfully converted
+		if(isNaN(q)){res.send("Not a valid id");res.end();} 
+		else{ next();}
+	},
+
+	function(req,res,next){
 	
-	// try to find the flight with the id
-	try{
-		fmodel.findOne({'fnum':q}).then((resp)=>{
-			if(resp!=null){
-				res.json(resp);
-			}
-			else{
-				res.send("No such flight number exists"); res.end();
-			}
-		});
+		// try to find the flight with the id
+		try{
+			fmodel.findOne({'fnum':q}).then((resp)=>{
+				if(resp!=null){
+					res.json(resp);
+				}
+				else{
+					res.send("No such flight number exists");
+				}
+			});
+		}
+		catch(err){
+			// console.log(err);
+			res.sendStatus(404); res.end();
+		}
 	}
-	catch(err){
-		// console.log(err);
-		res.sendStatus(404); res.end();
-	}
-	
-});
+);
 
 app.get('/',function(req,res){
 	res.send("Just a page");
